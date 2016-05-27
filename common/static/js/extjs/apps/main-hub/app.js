e782b675008455038c8c321f3b0c4a4f2cfa7e0f
anatskiy <evgeny.anatskiy@gmail.com>
2016-05-27 20:43:24 +0200

    trying to run the app

--- common/static/js/extjs/apps/main-hub/app.js
+++ common/static/js/extjs/apps/main-hub/app.js
@@ -6,7 +6,6 @@
 Ext.Loader.setConfig({
     enabled: true,
     paths: {
-        'MainHub': '/static/js/extjs/apps/main-hub',
         'MainHub.view.main.Main': '/static/js/extjs/apps/main-hub/app/view/main/Main.js',
         'MainHub.view.main.MainModel': '/static/js/extjs/apps/main-hub/app/view/main/MainModel.js',
         'MainHub.view.main.MainController': '/static/js/extjs/apps/main-hub/app/view/main/MainController.js',
@@ -23,11 +22,7 @@ Ext.application({
     extend: 'MainHub.Application',
 
     requires: [
-        'MainHub.view.main.Main',
-        'MainHub.view.main.MainModel',
-        'MainHub.view.main.MainController',
-        'MainHub.view.main.List',
-        'MainHub.store.Personnel'
+        'MainHub.view.main.Main'
     ],
 
     // The name of the initial view to create. With the classic toolkit this class
