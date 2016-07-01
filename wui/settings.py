dee71f50d85332bf4e54dc8fc7547bdf3bdb6e40
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-01 15:03:56 +0200

    fix static files path

--- wui/settings.py
+++ wui/settings.py
@@ -92,9 +92,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+]
 
 # WhiteNoise compression and caching support
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
