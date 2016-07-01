0f7901a0a2520007bbb1f59261c23a4a233bf6f1
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-01 16:50:55 +0200

    change STATIC_ROOT path

--- wui/settings.py
+++ wui/settings.py
@@ -110,7 +110,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
