3e8aa16ee1fa0e0d44adefa2a2c1e64278f1de65
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-01 14:29:51 +0200

    fix static files path

--- wui/settings.py
+++ wui/settings.py
@@ -89,16 +89,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
-]
-
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 try:
