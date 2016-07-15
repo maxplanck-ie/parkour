43a299b5296dbc3341c55606aae54a881f199bad
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 16:40:32 +0200

    add Ext JS production config

--- common/views.py
+++ common/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.conf import settings
 
 import json
 
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'DEBUG': settings.DEBUG})
 
 
+@login_required
 def get_username(request):
     user = request.user
     data = {}
-
     if user.is_authenticated():
         data['username'] = user.first_name + ' ' + user.last_name
-    else:
-        pass
-
     return HttpResponse(json.dumps({'data': data}), content_type='application/json')
