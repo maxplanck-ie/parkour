a041ef6183b86fea626161fa3311296a053942c1
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 14:37:29 +0200

    change STATIC_ROOT directory

--- wui/prod_settings.py
+++ wui/prod_settings.py
@@ -3,4 +3,4 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
