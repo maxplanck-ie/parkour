799d11c1b7b53e3e0e5d23d48b39c72131d270d6
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 12:28:18 +0200

    distinguish development and production configs

---
+++ wui/prod_settings.py
@@ -0,0 +1,6 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- wui/settings.py
+++ wui/settings.py
@@ -116,9 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
-]
+try:
+    from wui.dev_settings import *
+    DEBUG = True
+except ImportError:
+    from wui.prod_settings import *
+    DEBUG = False
