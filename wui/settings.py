ea9eaec0ee44b43c4864244349fb0f72c3ae6bac
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 14:47:58 +0200

    an attempt to fix finding static files on production

--- wui/settings.py
+++ wui/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 try:
     from wui.dev_settings import *
     DEBUG = True
