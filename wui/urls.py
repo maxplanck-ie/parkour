d85d504d38dc64e0497c9008e875690996c0b2d4
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 17:32:17 +0200

    clean up

--- wui/urls.py
+++ wui/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'', include('django.contrib.auth.urls')),
 
     url(r'', include('common.urls')),
     url(r'', include('researcher.urls')),
