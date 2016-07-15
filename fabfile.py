5a1caf37f2d50faefaceb480a6c63af625dd6ebe
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-15 11:45:27 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,5 @@ def deploy():
     branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
     container_id = local('docker ps -q -f name=parkourdocker_parkour_1', capture=True)
     local('docker exec -it %s /bin/bash' % container_id)
-    local('git checkout %s && git pull && exit' % branch_name)
+    # local('git checkout %s && git pull && exit' % branch_name)
     local('docker update --restart=always %s' % container_id)
