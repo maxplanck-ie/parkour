5fcefd48a432e3ce3aa663f72a9661f3b82100df
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-18 10:33:31 +0200

    set Fabric deployment on a remote host

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,17 @@
 from fabric.api import *
 import pyperclip
 
+import os
+
+env.hosts = [os.environ['FAB_HOST']]
+env.user = os.environ['FAB_USER']
+env.password = os.environ['FAB_PASS']
+
 
 def deploy():
     branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
     # container_id = local('docker ps -q -f name=parkourdocker_parkour_1', capture=True)
-    # local('docker exec -it %s /bin/bash' % container_id)
     pyperclip.copy('git checkout %s && git pull && '
                    'python manage.py collectstatic --noinput && exit' % branch_name)
-    local('docker exec -it parkourdocker_parkour_1 /bin/bash')
-    # local('docker update --restart=always %s' % container_id)
-    local('docker update --restart=always parkourdocker_parkour_1')
+    run('docker exec -it parkourdocker_parkour_1 /bin/bash')
+    run('docker restart parkourdocker_parkour_1')
