3b741a451abd8234834d32380df8b342764cada4
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-18 16:18:11 +0200

    write tests for 'request.models'

--- request/models.py
+++ request/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-
 from researcher.models import Researcher
 
 
 class Request(models.Model):
-    status = models.IntegerField()
+    status = models.IntegerField(null=True)
     name = models.CharField('Name', max_length=250)
-    project_type = models.CharField('Project Type', max_length=100)
-    date_created = models.DateField()
-    description = models.TextField()
-    researcher_id = models.ForeignKey(Researcher)
+    project_type = models.CharField('Project Type', max_length=100, null=True)
+    date_created = models.DateField(null=True)
+    description = models.TextField(null=True)
+    researcher_id = models.ForeignKey(Researcher, null=True)
     # sample_id = models.ForeignKey(Sample)
-    terms_of_use_accept = models.BooleanField()
+    terms_of_use_accept = models.BooleanField(default=True)
 
     def __str__(self):
         return '%s' % self.name
