9cec1f6c58b4f5abc5f791cfba33064eba327a6e
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-25 18:15:23 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,7 @@ def deploy():
     branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
     # container_id = local('docker ps -q -f name=parkourdocker_parkour_1', capture=True)
     pyperclip.copy('git checkout %s && git pull && '
-                   'python manage.py collectstatic --noinput && exit' % branch_name)
+                   'python manage.py migrate && python manage.py collectstatic --noinput && exit' % branch_name)
     run('docker exec -it parkourdocker_parkour_1 /bin/bash')
     run('docker restart parkourdocker_parkour_1')
 
