3a860c0b16c304f40ae21249838ee0e94ffa1a97
anatskiy <evgeny.anatskiy@gmail.com>
2016-08-19 15:45:32 +0200

    remove useless test

--- common/tests.py
+++ common/tests.py
@@ -18,14 +18,3 @@ class IndexViewTestCase(TestCase):
         response = self.client.get(reverse('index'), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-
-
-class GetUsernameTestCase(TestCase):
-    def setUp(self):
-        user = User.objects.create_user(username='foo', password='bar', first_name='Fooo')
-
-    def test_get_username(self):
-        self.client.login(username='foo', password='bar')
-        response = self.client.get(reverse('get_username'), follow=True)
-        user = User.objects.get(username='foo')
-        self.assertContains(response, user.first_name)
