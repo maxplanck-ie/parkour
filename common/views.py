c78b60bb1810bd953188628937b3b0e3ac0e3b7f
anatskiy <evgeny.anatskiy@gmail.com>
2016-08-31 21:24:36 +0200

    - change NavigationTree nodes - add drafts for further NavigationTree modification

--- common/views.py
+++ common/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.http import HttpResponse
+
+import json
 
 
 @login_required
@@ -8,5 +11,28 @@ def index(request):
     user = request.user
     return render(request, 'index.html', {
         'DEBUG': settings.DEBUG,
-        'USERNAME': '%s %s' % (user.first_name, user.last_name) if user.first_name else user.username
+        'USERNAME': '%s %s' % (user.first_name, user.last_name)
+        if user.first_name else user.username
     })
+
+
+def get_navigation_tree(request):
+    """ """
+    error = ''
+
+    data = [{
+        'text': 'Submission',
+        'iconCls': 'x-fa fa-tasks',
+        'expanded': True,
+        'selectable': False,
+        'children': []
+    }]
+
+    return HttpResponse(
+        json.dumps({
+            'success': not error,
+            'error': error,
+            'root': data,
+        }),
+        content_type='application/json',
+    )
