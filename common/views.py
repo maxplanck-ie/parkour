3b94972285027c1e2bbc91742eecd5c0a549408c
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2016-10-07 16:43:58 +0200

    get NavigationTree dynamically

--- common/views.py
+++ common/views.py
@@ -16,22 +16,66 @@ def index(request):
 
 
 def get_navigation_tree(request):
-    """ """
-    error = ''
-
-    data = [{
-        'text': 'Submission',
-        'iconCls': 'x-fa fa-tasks',
-        'expanded': True,
-        'selectable': False,
-        'children': []
-    }]
+    """ Get main NavigationTree """
+    data = {
+        "text": ".",
+        "children": [
+            {
+                'text': 'Start Page',
+                'iconCls': 'x-fa fa-th-large',
+                'viewType': 'startpage',
+                'leaf': True
+            },
+            # {
+            #     'text': 'Dashboard',
+            #     'iconCls': 'x-fa fa-desktop',
+            #     'viewType': 'dashboard',
+            #     'leaf': True
+            # },
+            # {
+            #     'text': 'Researchers',
+            #     'iconCls': 'x-fa fa-user',
+            #     'viewType': 'researchers',
+            #     'leaf': True
+            # },
+            {
+                'text': 'Submission',
+                'iconCls': 'x-fa fa-tasks',
+                'expanded': True,
+                'selectable': False,
+                'children': [
+                    {
+                        'text': 'Requests',
+                        'iconCls': 'x-fa fa-external-link-square',
+                        'viewType': 'requests',
+                        'leaf': True
+                    },
+                    {
+                        'text': 'Libraries/Samples',
+                        'iconCls': 'x-fa fa-flask',
+                        'viewType': 'libraries',
+                        'leaf': True
+                    }
+                ]
+            },
+            {
+                'text': 'Approval',
+                'iconCls': 'x-fa fa-check-square',
+                'expanded': True,
+                'selectable': False,
+                'children': [
+                    {
+                        'text': 'Incoming Libraries/Samples',
+                        'iconCls': 'x-fa fa-check',
+                        'viewType': 'incoming-libraries',
+                        'leaf': True
+                    }
+                ]
+            }
+        ]
+    }
 
     return HttpResponse(
-        json.dumps({
-            'success': not error,
-            'error': error,
-            'root': data,
-        }),
+        json.dumps(data),
         content_type='application/json',
     )
--- static/main-hub/app/store/NavigationTree.js
+++ static/main-hub/app/store/NavigationTree.js
@@ -3,81 +3,18 @@ Ext.define('MainHub.store.NavigationTree', {
 
     storeId: 'NavigationTree',
 
-    fields: [{ name: 'text' }],
+    proxy: {
+        type: 'ajax',
+        url: 'get_navigation_tree/',
+        timeout: 1000000,
+        pageParam: false,   //to remove param "page"
+        startParam: false,  //to remove param "start"
+        limitParam: false,  //to remove param "limit"
+        noCache: false,     //to remove param "_dc",
+        reader: {
+            type: 'json',
+        }
+    },
 
-    // proxy: {
-    //     type: 'ajax',
-    //     url: 'get_navigation_tree/',
-    //     timeout: 1000000,
-    //     pageParam: false,   //to remove param "page"
-    //     startParam: false,  //to remove param "start"
-    //     limitParam: false,  //to remove param "limit"
-    //     noCache: false,     //to remove param "_dc",
-    //     reader: {
-    //         type: 'json',
-    //         rootProperty: 'root',
-    //         successProperty: 'success'
-    //     }
-    // },
-
-    // autoLoad: true,
-
-    root: {
-        expanded: true,
-        children: [
-            {
-                text: 'Start Page',
-                iconCls: 'x-fa fa-th-large',
-                viewType: 'startpage',
-                leaf: true
-            },
-            // {
-            //     text: 'Dashboard',
-            //     iconCls: 'x-fa fa-desktop',
-            //     viewType: 'dashboard',
-            //     // routeId: 'dashboard', // routeId defaults to viewType
-            //     leaf: true
-            // },
-            // {
-            //     text: 'Researchers',
-            //     iconCls: 'x-fa fa-user',
-            //     viewType: 'researchers',
-            //     leaf: true
-            // },
-            {
-                text: 'Submission',
-                iconCls: 'x-fa fa-tasks',
-                expanded: true,
-                selectable: false,
-                children: [
-                    {
-                        text: 'Requests',
-                        iconCls: 'x-fa fa-external-link-square',
-                        viewType: 'requests',
-                        leaf: true
-                    },
-                    {
-                        text: 'Libraries/Samples',
-                        iconCls: 'x-fa fa-flask',
-                        viewType: 'libraries',
-                        leaf: true
-                    }
-                ]
-            },
-            {
-                text: 'Approval',
-                iconCls: 'x-fa fa-check-square',
-                expanded: true,
-                selectable: false,
-                children: [
-                    {
-                        text: 'Incoming Libraries/Samples',
-                        iconCls: 'x-fa fa-check',
-                        viewType: 'incoming-libraries',
-                        leaf: true
-                    }
-                ]
-            }
-        ]
-    }
+    autoLoad: true
 });
