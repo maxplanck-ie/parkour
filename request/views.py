5896a80a29c58fde22b6091a241a9c850f406ab1
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2016-10-08 16:01:11 +0200

    generate "correct" PDF

--- request/views.py
+++ request/views.py
@@ -185,6 +185,17 @@ def get_libraries_in_request(request):
     )
 
 
+def draw_string(p, x, y, string):
+    _x, _y = x, y
+    _y -= 30
+    p.drawString(_x, _y, string[0])
+    _x += 25
+    p.drawString(_x, _y, string[1])
+    _x += 300
+    p.drawString(_x, _y, string[2])
+    _x += 70
+    p.drawString(_x, _y, string[3])
+
 @csrf_exempt
 def generate_pdf(request):
     """ """
@@ -194,19 +205,95 @@ def generate_pdf(request):
     response['Content-Disposition'] =\
         'inline; filename="PI_Approval_blank.pdf"'
 
-    p = canvas.Canvas(response)
-
     try:
         req = Request.objects.get(id=request_id)
-        PAGE_HEIGHT=defaultPageSize[1]  
-        PAGE_WIDTH=defaultPageSize[0] 
-        p.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-100, 'Approval Blank') 
-        p.drawString(inch, 10*inch-20, 'Request Name: ' + req.name)
-        p.drawString(inch, 10*inch-35, 'Description: ' + req.description)
-        p.drawString(inch, 10*inch-50, 'Submitted Libraries/Samples:')
+
+        p = canvas.Canvas(response)
+        PAGE_HEIGHT=defaultPageSize[1]
+        PAGE_WIDTH=defaultPageSize[0]
+        FONT_BOLD = 'Helvetica-Bold'
+        FONT = 'Helvetica'
+        HEADER_FONT_SIZE = 14
+        DEFAULT_FONT_SIZE = 12
+        SMALL_FONT_SIZE = 10
+        x = inch
+        y = 10 * inch
+
+        # Strings
+        title = 'Approval Blank'
+        request_name_label = 'Request name: '
+        request_name = req.name
+        description_label = 'Description: '
+        description = req.description
+        submitted_libraries_samples = 'Submitted Libraries/Samples: '
+        page_1 = 'Page 1 of 2'
+        page_2 = 'Page 2 of 2'
+
+        # Page 1
+        _x = x + 100
+        _y = y - 20
+        p.setFont(FONT_BOLD, HEADER_FONT_SIZE)
+        p.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-100, title)
+        p.setFont(FONT_BOLD, DEFAULT_FONT_SIZE)
+        p.drawString(x, _y, request_name_label)
+        p.setFont(FONT, DEFAULT_FONT_SIZE)
+        p.drawString(_x, _y, request_name)
+        _y -= 15
+        p.setFont(FONT_BOLD, DEFAULT_FONT_SIZE)
+        p.drawString(x, _y, description_label)
+        p.setFont(FONT, DEFAULT_FONT_SIZE)
+        p.drawString(_x, _y, description)   # doesn't fit (if it's too long)
+        p.setFont(FONT, SMALL_FONT_SIZE)
+        p.drawString(x*6.5, inch, page_1)
+        p.showPage()
+
+        # Page 2
+        _y = y - 20
+        p.setFont(FONT_BOLD, HEADER_FONT_SIZE)
+        p.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-100, title)
+        p.setFont(FONT_BOLD, DEFAULT_FONT_SIZE)
+        p.drawString(x, _y, submitted_libraries_samples)
+        p.setFont(FONT_BOLD, SMALL_FONT_SIZE)
+        draw_string(p, x, y-20, ('#', 'Name', 'Type', 'Barcode'))
+        p.setFont(FONT, SMALL_FONT_SIZE)
+
+        libraries = [
+            {
+                'name': library.name,
+                'type': 'Library',
+                'barcode': library.barcode,
+            }
+            for library in req.libraries.all()
+        ]
+        samples = [
+            {
+                'name': sample.name,
+                'type': 'Sample',
+                'barcode': sample.barcode,
+            }
+            for sample in req.samples.all()
+        ]
+        data = sorted(
+            libraries + samples,
+            key=lambda x: (x['type'], x['name']),
+            reverse=True,
+        )
+
+        # Only ~55 records fit into the page
+        for i, record in enumerate(data):
+            draw_string(
+                p,
+                x,
+                y - (30 + (i + 1) * 10),
+                (str(i+1), record['name'], record['type'], record['barcode']),
+            )
+
+        p.drawString(x*6.5, inch, page_2)
+
     except:
         # TODO: Error handling
         pass
+
     finally:
         p.showPage()
         p.save()
