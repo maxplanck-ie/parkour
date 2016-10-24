0c6ca150cbb0391b94bd15e4831486e696f6b173
anatskiy <evgeny.anatskiy@gmail.com>
2016-10-24 14:16:50 +0200

    changes

--- static/main-hub/app/view/pooling/Pooling.js
+++ static/main-hub/app/view/pooling/Pooling.js
@@ -9,9 +9,44 @@ Ext.define('MainHub.view.pooling.Pooling', {
 
     controller: 'pooling',
 
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
+    },
+    padding: 15,
+
+    defaultType: 'panel',
     items: [
         {
-            padding: 15
+            title: 'Libraries for Pooling',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            flex: 2,
+            margin: '0 15px 0 0',
+
+            items: [
+                {
+                    html: 'Container 1',
+                    border: 0,
+                    padding: 15
+                }
+            ]
+        },
+        {
+            title: 'Graphical Representation',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            flex: 1,
+
+            items: [{
+                html: 'Container 2',
+                border: 0,
+                padding: 15
+            }]
         }
     ]
 });
