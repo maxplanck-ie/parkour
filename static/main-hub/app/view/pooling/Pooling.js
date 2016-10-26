8f3575e97dc835df8a6e03735f8475025ebb0b04
anatskiy <evgeny.anatskiy@gmail.com>
2016-10-26 12:27:42 +0200

    add some logic

--- static/main-hub/app/view/pooling/Pooling.js
+++ static/main-hub/app/view/pooling/Pooling.js
@@ -14,144 +14,196 @@ Ext.define('MainHub.view.pooling.Pooling', {
     },
     padding: 15,
 
-    defaultType: 'panel',
-    items: [
-        {
-            xtype: 'treepanel',
-            itemId: 'poolingTreePanel',
-            height: Ext.Element.getViewportHeight() - 94,
-            margin: '0 15px 0 0',
-            flex: 1,
-            header: {
-                title: 'Libraries for Pooling',
-                items: [
-                    {
-                        xtype: 'combobox',
-                        itemId: 'poolSize',
-                        queryMode: 'local',
-                        displayField: 'name',
-                        valueField: 'value',
-                        // emptyText: '25 M',
-                        forceSelection: true,
-                        cls: 'panel-header-combobox',
-                        fieldLabel: 'Pool Size',
-                        labelWidth: 65,
-                        width: 170,
+    initComponent: function() {
+        var me = this;
 
-                        store: Ext.create('Ext.data.Store', {
-                            fields: [
-                                { name: 'value', type: 'int'    },
-                                { name: 'name',  type: 'string' }
-                            ],
-                            data: [
-                                { value: 25,   name: '25 M'   },
-                                { value: 130,  name: '130 M'  },
-                                { value: 200,  name: '200 M'  },
-                                { value: 400,  name: '400 M'  },
-                                { value: 600,  name: '600 M'  },
-                                { value: 800,  name: '800 M'  },
-                                { value: 1000, name: '1000 M' },
-                                { value: 1200, name: '1200 M' },
-                                { value: 1400, name: '1400 M' },
-                                { value: 1600, name: '1600 M' }
-                            ]
-                        })
+        me.items = [
+            {
+                xtype: 'treepanel',
+                itemId: 'poolingTreePanel',
+                height: Ext.Element.getViewportHeight() - 94,
+                margin: '0 15px 0 0',
+                flex: 1,
+                header: {
+                    title: 'Libraries for Pooling',
+                    items: [
+                        {
+                            xtype: 'combobox',
+                            id: 'poolSize',
+                            itemId: 'poolSize',
+                            queryMode: 'local',
+                            displayField: 'name',
+                            valueField: 'value',
+                            forceSelection: true,
+                            cls: 'panel-header-combobox',
+                            fieldLabel: 'Pool Size',
+                            labelWidth: 65,
+                            width: 170,
+
+                            store: Ext.create('Ext.data.Store', {
+                                fields: [
+                                    { name: 'value', type: 'int'    },
+                                    { name: 'name',  type: 'string' }
+                                ],
+                                data: [
+                                    { value: 25,   name: '25 M'   },
+                                    { value: 130,  name: '130 M'  },
+                                    { value: 200,  name: '200 M'  },
+                                    { value: 400,  name: '400 M'  },
+                                    { value: 600,  name: '600 M'  },
+                                    { value: 800,  name: '800 M'  },
+                                    { value: 1000, name: '1000 M' },
+                                    { value: 1200, name: '1200 M' },
+                                    { value: 1400, name: '1400 M' },
+                                    { value: 1600, name: '1600 M' }
+                                ]
+                            })
+                        }
+                    ]
+                },
+                store: 'PoolingTree',
+                rootVisible: false,
+                columns: [
+                    {
+                        xtype: 'treecolumn',
+                        text: 'Library Name',
+                        dataIndex: 'text',
+                        sortable: true,
+                        flex: 1
+                    },
+                    {
+                        text: 'Seq. Depth',
+                        dataIndex: 'sequencingDepth',
+                        width: 90
+                    },
+                    {
+                        text: 'Read Length',
+                        dataIndex: 'sequencingRunConditionName',
+                        width: 100
+                    },
+                    {
+                        text: 'Index Type',
+                        dataIndex: 'indexTypeName',
+                        width: 90
+                    },
+                    {
+                        text: 'Index I7',
+                        dataIndex: 'indexI7',
+                        width: 100
+                    },
+                    {
+                        text: 'Index I5',
+                        dataIndex: 'indexI5',
+                        width: 100
+                    }
+                ],
+                bbar: [
+                    '->',
+                    {
+                        xtype: 'button',
+                        itemId: 'createPool',
+                        text: 'Pool'
                     }
                 ]
             },
-            store: 'PoolingTree',
-            rootVisible: false,
-            columns: [
-                {
-                    xtype: 'treecolumn',
-                    text: 'Library Name',
-                    dataIndex: 'text',
-                    sortable: true,
-                    flex: 1
+            {
+                xtype: 'grid',
+                id: 'poolGrid',
+                itemId: 'poolGrid',
+                header: {
+                    title: 'Pool',
+                    height: 56
                 },
-                {
-                    text: 'Seq. Depth',
-                    dataIndex: 'sequencingDepth',
-                    width: 90
-                },
-                {
-                    text: 'Read Length',
-                    dataIndex: 'sequencingRunConditionName',
-                    width: 100
-                },
-                {
-                    text: 'Index Type',
-                    dataIndex: 'indexTypeName',
-                    width: 90
-                },
-                {
-                    text: 'Index I7',
-                    dataIndex: 'indexI7',
-                    width: 100
-                },
-                {
-                    text: 'Index I5',
-                    dataIndex: 'indexI5',
-                    width: 100
-                }
-            ],
-            bbar: [
-                '->',
-                {
-                    xtype: 'button',
-                    itemId: 'createPool',
-                    text: 'Pool'
-                }
-            ]
-        },
-        {
-            xtype: 'grid',
-            header: {
-                title: 'Pool',
-                height: 56
-            },
-            height: Ext.Element.getViewportHeight() - 94,
-            flex: 1,
-            
-            bbar: [
-                '->',
-                {
-                    xtype: 'button',
-                    itemId: 'savePool',
-                    text: 'Save',
-                    disabled: true
-                }
-            ]
-        },
-        // {
-        //     header: {
-        //         title: 'Pooling',
-        //         height: 56
-        //     },
-        //     layout: {
-        //         type: 'vbox',
-        //         align: 'stretch'
-        //     },
-        //     flex: 1,
-        //     // width: 350,
-        //     items: [{
-        //         html: `
-        //             <div style="font-family:Monospace;font-size:25px;">
-        //                 <span style="color:green">G</span>
-        //                 <span style="color:red">C</span>
-        //             <div>
-        //         `,
-        //         border: 0,
-        //         padding: 15
-        //     }],
-        //     bbar: [
-        //         '->',
-        //         {
-        //             xtype: 'button',
-        //             itemId: 'savePool',
-        //             text: 'Pool'
-        //         }
-        //     ]
-        // }
-    ]
+                height: Ext.Element.getViewportHeight() - 94,
+                flex: 1,
+                columns: [
+                    {
+                        text: 'Name',
+                        dataIndex: 'name',
+                        flex: 1
+                    },
+                    {
+                        text: 'Seq. Depth',
+                        dataIndex: 'sequencingDepth',
+                        width: 90
+                    },
+                    {
+                        text: 'Index I7 ID',
+                        dataIndex: 'indexI7Id',
+                        width: 90
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_1',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_2',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_3',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_4',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_5',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_6',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_7',
+                        renderer: me.renderCell,
+                        width: 30
+                    },
+                    {
+                        text: '',
+                        dataIndex: 'indexI7_8',
+                        renderer: me.renderCell,
+                        width: 30
+                    }
+                ],
+                store: [],
+                bbar: [
+                    '->',
+                    {
+                        xtype: 'button',
+                        itemId: 'savePool',
+                        text: 'Save',
+                        disabled: true
+                    }
+                ],
+                html: '<div class="myText">!!! TEXT !!!</div>'
+            }
+        ];
+
+        me.callParent(arguments);
+    },
+
+    renderCell: function(val, meta) {
+        if (val == 'G' || val == 'T') {
+            meta.tdStyle = 'background-color:#dcedc8';
+        } else if (val == 'A' || val == 'C') {
+            meta.tdStyle = 'background-color:#ef9a9a';
+        }
+        return val;
+    }
 });
--- static/main-hub/app/view/pooling/PoolingController.js
+++ static/main-hub/app/view/pooling/PoolingController.js
@@ -10,16 +10,101 @@ Ext.define('MainHub.view.pooling.PoolingController', {
                 checkchange: 'onPoolingTreePanelCheckchange'
             },
             '#poolSize': {
-                boxready: 'onPoolSizeBoxready'
-            }
+                boxready: 'onMaxPoolSizeBoxready'
+            },
+            '#poolGrid': {}
         }
     },
 
-    onPoolSizeBoxready: function(cb) {
+    onMaxPoolSizeBoxready: function(cb) {
         cb.select(25, true);
     },
 
     onPoolingTreePanelCheckchange: function(node, checked) {
-        // TODO: Check if indices are unique and compatible
+        var grid = Ext.getCmp('poolGrid'),
+            store = grid.getStore();
+
+        if (checked) {
+            if (this.isUnique(store, node) && this.isCompatible(store, node) &&
+                this.isColorDiversityMaximized(store, node) && this.isPoolSizeOk(store, node)) {
+
+                var indexI7Sequence = node.get('indexI7'),
+                    indexI7 = indexI7Sequence.split('');
+                if (indexI7Sequence.length == 6) {
+                    $.merge(indexI7, [' ', ' ']);
+                }
+
+                store.add({
+                    name: node.get('text'),
+                    libraryId: node.get('libraryId'),
+                    sequencingDepth: node.get('sequencingDepth'),
+                    sequencingRunCondition: node.get('sequencingRunCondition'),
+                    indexI7: indexI7Sequence,
+                    indexI7Id: '',
+                    indexI7_1: indexI7[0],
+                    indexI7_2: indexI7[1],
+                    indexI7_3: indexI7[2],
+                    indexI7_4: indexI7[3],
+                    indexI7_5: indexI7[4],
+                    indexI7_6: indexI7[5],
+                    indexI7_7: indexI7[6],
+                    indexI7_8: indexI7[7]
+                });
+
+                var content = Ext.dom.Query.selectNode('.myText'),
+                    rowsContainer = grid.getEl().select('.x-grid-item-container').elements[0];
+
+                rowsContainer.append(content);
+            } else {
+                node.set('checked', false);
+            }
+        } else {
+            var record = store.findRecord('libraryId', node.get('libraryId'));
+            store.remove(record);
+        }
+    },
+
+    isUnique: function(store, node) {
+        if (store.getCount()) {
+            var record = store.findRecord('indexI7', node.get('indexI7'));
+            if (record) {
+                Ext.ux.ToastMessage('The selected index is already in the pool.', 'warning');
+                return false;
+            }
+        }
+        return true;
+    },
+
+    isCompatible: function(store, node) {
+        if (store.getCount()) {
+            // Same Read Length
+            var record = store.getAt(0);
+            if (record.get('sequencingRunCondition') != node.get('sequencingRunCondition')) {
+                Ext.ux.ToastMessage('Read lengths must be the same.', 'warning');
+                return false;
+            }
+        }
+        return true;
+    },
+
+    isColorDiversityMaximized: function(store, node) {
+        return true;
+    },
+
+    isPoolSizeOk: function(store, node) {
+        var maxPoolSize = Ext.getCmp('poolSize').getValue(),
+            poolSize = 0;
+
+        store.each(function(record) {
+            poolSize += record.get('sequencingDepth');
+        });
+        poolSize += node.get('sequencingDepth');
+
+        if (poolSize >= maxPoolSize) {
+            Ext.ux.ToastMessage('You have exceeded Pool Size.<br>Please increase it.', 'warning');
+            // return false;
+        }
+
+        return true;
     }
 });
