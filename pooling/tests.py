70dddd5b8b86e42e22cb5def1012b928d6b9700b
anatskiy <evgeny.anatskiy@gmail.com>
2016-12-12 16:52:17 +0100

    add a Model test for the pooling app

--- pooling/tests.py
+++ pooling/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Pooling
+from library.models import Library
+
+
+# Models
+
+class PoolingTest(TestCase):
+    def setUp(self):
+        self.library = Library.get_test_library('Library')
+        self.pooling = Pooling(library=self.library)
+
+    def test_pooling_name(self):
+        self.assertTrue(isinstance(self.pooling, Pooling))
+        self.assertEqual(self.pooling.__str__(), self.library.name)
