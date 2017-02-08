e80074c6d8ac9208a3b34e96dbd7bfe528a38314
anatskiy <evgeny.anatskiy@gmail.com>
2017-02-08 17:20:51 +0100

    [WIP] add new Index Generator

---
+++ index_generator/index_generator.py
@@ -0,0 +1,399 @@
+import re
+import random
+
+from library_sample_shared.models import IndexI7, IndexI5
+from library.models import Library
+from sample.models import Sample
+
+
+class IndexGenerator:
+    """ Main IndexGenerator class. """
+
+    def __init__(self, library_ids=[], sample_ids=[]):
+        self._result = []
+
+        self._libraries = Library.objects.filter(id__in=library_ids)
+        self._samples = Sample.objects.filter(id__in=sample_ids)
+        self._num_libraries = self._libraries.count()
+        self._num_samples = self._samples.count()
+
+        if self._num_samples == 0:
+            raise ValueError('No samples.')
+
+        self.construct_index_registry()
+
+    def construct_index_registry(self):
+        """
+        Construct a dictionary with all libraries' and samples' indices.
+        """
+        self._indices = {}
+
+        # Index types for all libraries and samples
+        index_types = list(set(
+            [l.index_type for l in self._libraries if l.index_type] +
+            [s.index_type for s in self._samples if s.index_type]
+        ))
+
+        if not index_types:
+            raise ValueError('Index Type must be set for all ' +
+                             'libraries and samples.')
+
+        for index_type in index_types:
+            self._indices[index_type.pk] = {'i7': [], 'i5': []}
+
+            if index_type.is_index_i7:
+                self._indices[index_type.pk]['i7'].extend([
+                    {
+                        'index': idx.index,
+                        'index_id': idx.index_id,
+                    }
+                    for idx in IndexI7.objects.filter(
+                        index_type=index_type.pk,
+                    )
+                ])
+
+            if index_type.is_index_i5:
+                self._indices[index_type.pk]['i5'].extend([
+                    {
+                        'index': idx.index,
+                        'index_id': idx.index_id,
+                    }
+                    for idx in IndexI5.objects.filter(
+                        index_type=index_type.pk,
+                    )
+                ])
+
+    def update_indices(self, idx_type_id, idx_type, index):
+        """ Remove a given index from the index registry. """
+        self._indices[idx_type_id][idx_type].pop(
+            self._indices[idx_type_id][idx_type].index(index)
+        )
+
+    @staticmethod
+    def convert_index(index):
+        """ Convert A/C into R (red) and T into G (green). """
+        return re.sub('T', 'G', re.sub('A|C', 'R', index))
+
+    def best_pair_scores(self, index1, index2, depth1, depth2):
+        """
+        Calculate the score for two given indices.
+
+        Score is an absolute difference between the sequence depths of
+        the two indices divided by the total sequence depth (in %).
+
+        The ideal score is 0.0 (50% green and 50% red),
+        an acceptable score is 60.0 (80%/20% or 20%/80%).
+
+        If the score > 60%, then the indices are not compatible.
+        """
+        index1 = self.convert_index(index1)
+        index2 = self.convert_index(index2)
+        total_depth = depth1 + depth2
+        result = []
+
+        for cycle in range(6):
+            if index1[cycle] != index2[cycle]:
+                difference = abs(depth1 - depth2) / total_depth * 100
+                if difference <= 60.0:
+                    result.append(difference)
+                else:
+                    # If color diversity is low (81%/19% or worse) in
+                    # one of the cycles
+                    return -1
+            else:
+                # If nucleotides are only red (A/C) or green (G/T) in a cycle
+                return -1
+
+        return result
+
+    def calculate_scores_n(self, current_record, current_index):
+        """
+        Calculate score for N given libraries/samples.
+
+        The scoring principle is the same as in best_pair_scores() but
+        for N libraries and samples.
+        """
+        result = []
+
+        # Calculate color distribution for libraries
+        # which are already in the result
+        color_distribution = [{'G': 0, 'R': 0} for _ in range(6)]
+        total_depth = 0
+        for record in self._result:
+            for cycle in range(6):
+                index = self.convert_index(record['index_i7']['index'])
+                color = index[cycle]
+                color_distribution[cycle][color] += record['depth']
+            total_depth += record['depth']
+        total_depth += current_record.sequencing_depth
+
+        # Calculate scores for the libraries in the result with a new library
+        for cycle in range(6):
+            index = self.convert_index(current_index['index'])
+            color = index[cycle]
+            color_distribution[cycle][color] += current_record.sequencing_depth
+
+            if color_distribution[cycle]['G'] > 0 and \
+                    color_distribution[cycle]['R'] > 0:
+                difference = abs((color_distribution[cycle]['G'] -
+                                  color_distribution[cycle]['R']) /
+                                 total_depth) * 100
+
+                if difference <= 60.0:
+                    result.append(difference)
+                else:
+                    # If color diversity is low (81%/19% or worse) in
+                    # one of the cycles
+                    return -1
+            else:
+                # If nucleotides are only red (A/C) or green (G/T) in a cycle
+                return -1
+
+        return result
+
+    @staticmethod
+    def create_result_dict(sample, index_i7, index_i5):
+        """ Create a dictionary with generated sample indices. """
+        return {
+            'name': sample.name,
+            'sample_id': sample.pk,
+            'read_length': sample.read_length_id,
+            'depth': sample.sequencing_depth,
+            'index_i7': index_i7,
+            'index_i5': index_i5,
+        }
+
+    @staticmethod
+    def create_index_dict(index='', index_id=''):
+        return {
+            'index': index,
+            'index_id': index_id,
+        }
+
+    def find_best_pair(self):
+        """
+        Find a pair of best matching sample indices (I7) and
+        add it to the result.
+        """
+        sample1 = self._samples[0]
+        sample2 = self._samples[1]
+
+        # TODO@me: ensure index_type != ''
+        best_pair = {'avg_score': 100.0}  # the worst score
+        for index1 in self._indices[sample1.index_type.pk]['i7']:
+            for index2 in self._indices[sample2.index_type.pk]['i7']:
+                if index1 != index2:
+                    scores = self.best_pair_scores(
+                        index1['index'],
+                        index2['index'],
+                        sample1.sequencing_depth,
+                        sample2.sequencing_depth,
+                    )
+
+                    # If both indices are compatible
+                    if scores != -1:
+                        avg_score = sum(scores) / 6
+                        if avg_score < best_pair['avg_score']:
+                            best_pair = {
+                                'index1': index1,
+                                'index2': index2,
+                                'avg_score': avg_score,
+                            }
+
+        # If the best pair was found
+        if 'index1' in best_pair.keys():
+            self._result.extend([
+                self.create_result_dict(
+                    sample1,
+                    best_pair['index1'],
+                    self.create_index_dict('', ''),  # Index I5
+                ),
+                self.create_result_dict(
+                    sample2,
+                    best_pair['index2'],
+                    self.create_index_dict('', ''),  # Index I5
+                ),
+            ])
+
+            self.update_indices(
+                sample1.index_type.id, 'i7', best_pair['index1'],
+            )
+
+            self.update_indices(
+                sample2.index_type.id, 'i7', best_pair['index2'],
+            )
+        else:
+            raise ValueError('Could not find the best ' +
+                             'matching pair of indices.')
+
+    def generate_index_i7(self, sample, i):
+        """ Generate index I7 for a given sample. """
+        index_i7 = {'avg_score': 100.0}
+
+        for index in self._indices[sample.index_type.id]['i7']:
+            scores = self.calculate_scores_n(sample, index)
+
+            # If indices are compatible
+            if scores != -1:
+                indices_in_result = [
+                    res['index_i7']['index']
+                    for res in self._result
+                ]
+                avg_score = sum(scores) / 6
+
+                # Choose an index with a better average score
+                # and make sure it's not in the result
+                if avg_score < index_i7['avg_score'] \
+                        and index['index'] not in indices_in_result:
+                    index_i7 = {
+                        'index': index,
+                        'avg_score': avg_score
+                    }
+
+        # If an index has been generated
+        if 'index' in index_i7.keys():
+            self._result.append(self.create_result_dict(
+                sample,
+                index_i7['index'],
+                self.create_index_dict('', ''),  # Index I5
+            ))
+
+            self.update_indices(sample.index_type.id, 'i7', index)
+
+        else:
+            if i + 1 == self._num_samples:
+                # Given libraries, if an index cannot be found
+                # for the last sample, raise an exception
+                raise ValueError('Could not generate indices.')
+            else:
+                # Choose a random index and try to generate indices
+                # for the remaining samples
+                index_i7 = random.choice(
+                    self._indices[sample.index_type.id]['i7'],
+                )
+
+                self._result.append(self.create_result_dict(
+                    sample,
+                    index_i7,
+                    self.create_index_dict('', ''),  # Index I5
+                ))
+
+                self.update_indices(sample.index_type.id, 'i7', index_i7)
+
+    def add_libraries_to_result(self):
+        """ Add all libraries directly to the result. """
+        for library in self._libraries:
+            # Get Index I7
+            index_i7 = IndexI7.objects.filter(
+                index=library.index_i7,
+                index_type=library.index_type,
+            )
+
+            if index_i7:
+                index_i7 = self.create_index_dict(index_i7[0].index,
+                                                  index_i7[0].index_id)
+            else:
+                index_i7 = self.create_index_dict(library.index_i7, '')
+
+            # Get Index I5
+            index_i5 = IndexI5.objects.filter(
+                index=library.index_i5,
+                index_type=library.index_type,
+            )
+
+            if index_i5:
+                index_i5 = self.create_index_dict(index_i5[0].index,
+                                                  index_i5[0].index_id)
+            else:
+                idx_i5 = library.index_i5 if library.index_i5 else ''
+                index_i5 = self.create_index_dict(idx_i5, '')
+
+            self._result.append({
+                'name': library.name,
+                'library_id': library.id,
+                'read_length': library.read_length_id,
+                'depth': library.sequencing_depth,
+                'index_i7': index_i7,
+                'index_i5': index_i5,
+            })
+
+            # If Index I7 isn't in the database, don't raise an exception
+            try:
+                self.update_indices(library.index_type.id, 'i7', index_i7)
+            except ValueError:
+                pass
+
+            # If Index I5 isn't in the database, don't raise an exception
+            try:
+                self.update_indices(library.index_type.id, 'i5', index_i5)
+            except ValueError:
+                pass
+
+    def prepare_result(self):
+        """
+        Construct the final dictionary with all records and their indices.
+        """
+        result = []
+
+        for record in self._result:
+            index_i7 = record['index_i7']
+            index_i5 = record['index_i5']
+
+            rec = {
+                'name': record['name'],
+                'sequencingDepth': record['depth'],
+                'readLength': record['read_length'],
+                'indexI7Id': index_i7['index_id'],
+                'indexI5Id': index_i5['index_id'],
+            }
+
+            if 'sample_id' in record.keys():
+                rec.update({
+                    'recordType': 'S',
+                    'sampleId': record['sample_id']
+                })
+
+            if 'library_id' in record.keys():
+                rec.update({
+                    'recordType': 'L',
+                    'libraryId': record['library_id']
+                })
+
+            for i in range(len(index_i7['index'])):
+                rec.update({'indexI7_' + str(i + 1): index_i7['index'][i]})
+
+            for i in range(len(index_i5['index'])):
+                rec.update({'indexI5_' + str(i + 1): index_i5['index'][i]})
+
+            result.append(rec)
+
+        return result
+
+    def generate(self):
+        """ Main class method for generating indices. """
+        if self._num_libraries > 0:
+            # Add all libraries directly to the result
+            case = 1
+            self.add_libraries_to_result()
+        else:
+            # Generate indices only for samples
+            case = 2
+            if self._num_samples > 1:
+                self.find_best_pair()
+            else:
+                raise ValueError('Select at least two samples.')
+
+        if case == 1:
+            # If there are any libraries, consider all samples
+            start = 0
+        else:
+            # case 2: generate indices for all samples,
+            # except for the first two
+            start = 2
+
+        for i, sample in enumerate(self._samples[start:self._num_samples]):
+            self.generate_index_i7(sample, i)
+            # self.generate_index_i5(sample, i)
+
+        result = self.prepare_result()
+        return result
--- index_generator/tests.py
+++ index_generator/tests.py
@@ -1,3 +1,177 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+
+from .models import Pool
+from library_sample_shared.models import IndexType, IndexI7, IndexI5
+from library.models import Library
+from sample.models import Sample
+
+User = get_user_model()
+
+
+# Models
+
+class PoolTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name='Foo',
+            last_name='Bar',
+            email='foo@bar.io',
+            password='foo-foo',
+        )
+        self.pool = Pool(user=self.user)
+        self.pool.save()
+
+    def test_pool_name(self):
+        self.assertTrue(isinstance(self.pool, Pool))
+        self.assertEqual(self.pool.__str__(), self.pool.name)
+
+
+# Views
+
+class GenerateIndicesTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(email='foo@bar.io', password='foo-foo')
+
+        # Index Types
+        idx_type1 = IndexType(name='Index Type 1', is_index_i7=True)
+        idx_type2 = IndexType(name='Index Type 2', is_index_i7=True)
+        idx_type3 = IndexType(
+            name='Index Type 3',
+            is_index_i7=True,
+            is_index_i5=True,
+        )
+        idx_type1.save()
+        idx_type2.save()
+        idx_type3.save()
+
+        # Indices I7
+        index1 = IndexI7(index_type=idx_type1, index='ATCACG', index_id='A01')
+        index2 = IndexI7(index_type=idx_type1, index='CGATGT', index_id='A02')
+        index3 = IndexI7(index_type=idx_type1, index='ACAGTG', index_id='A05')
+        index4 = IndexI7(index_type=idx_type1, index='GTGGCC', index_id='A20')
+        index5 = IndexI7(index_type=idx_type1, index='GAGTGG', index_id='A23')
+        index6 = IndexI7(index_type=idx_type1, index='ATTCCT', index_id='A27')
+        index7 = IndexI7(index_type=idx_type1, index='ATGTCA', index_id='A15')
+        index8 = IndexI7(index_type=idx_type1, index='AGTCAA', index_id='A13')
+        index9 = IndexI7(index_type=idx_type1, index='GTCCGC', index_id='A18')
+        index10 = IndexI7(index_type=idx_type1, index='GTGAAA', index_id='A19')
+        index11 = IndexI7(index_type=idx_type2, index='ATCACG', index_id='RP1')
+        index12 = IndexI7(index_type=idx_type2, index='CAACTA', index_id='RP29')
+        index13 = IndexI7(index_type=idx_type2, index='TACAGC', index_id='RP43')
+        index14 = IndexI7(index_type=idx_type2, index='GGTAGC', index_id='RP24')
+        index15 = IndexI7(index_type=idx_type2, index='GTGAAA', index_id='RP19')
+
+        index1.save()
+        index2.save()
+        index3.save()
+        index4.save()
+        index5.save()
+        index6.save()
+        index7.save()
+        index8.save()
+        index9.save()
+        index10.save()
+        index11.save()
+        index12.save()
+        index13.save()
+        index14.save()
+        index15.save()
+
+        # Libraries
+        self.library1 = Library.get_test_library('Library1')
+        self.library2 = Library.get_test_library('Library2')
+        self.library1.index_type = idx_type1
+        self.library1.index_i7 = 'ATCACG'
+        self.library2.index_i7 = 'ATCGAT'
+        self.library1.save()
+        self.library2.save()
+
+        # Samples
+        self.sample1 = Sample.get_test_sample('Sample1')
+        self.sample2 = Sample.get_test_sample('Sample2')
+        self.sample3 = Sample.get_test_sample('Sample3')
+        self.sample4 = Sample.get_test_sample('Sample4')
+        self.sample5 = Sample.get_test_sample('Sample5')
+        self.sample1.index_type = idx_type1
+        self.sample2.index_type = idx_type1
+        self.sample3.index_type = idx_type2
+        self.sample1.save()
+        self.sample2.save()
+        self.sample3.save()
+        self.sample4.save()
+        self.sample5.save()
+
+    def test_generate_indices(self):
+        self.client.login(email='foo@bar.io', password='foo-foo')
+        libraries = ','.join(
+            [str(self.library1.pk), str(self.library2.pk)],
+        )
+        samples = ','.join(
+            [str(self.sample1.pk), str(self.sample2.pk), str(self.sample3.pk)],
+        )
+        response = self.client.post(reverse('generate_indices'), {
+            'libraries': '[%s]' % libraries,
+            'samples': '[%s]' % samples,
+        })
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(str(response.content, 'utf-8'))
+        self.assertEqual(content['success'], True)
+        self.assertGreater(len(content['data']), 0)
+
+    def test_generate_indices_samples_only(self):
+        self.client.login(email='foo@bar.io', password='foo-foo')
+        samples = ','.join(
+            [str(self.sample1.pk), str(self.sample2.pk), str(self.sample3.pk)],
+        )
+        response = self.client.post(reverse('generate_indices'), {
+            'samples': '[%s]' % samples,
+        })
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(str(response.content, 'utf-8'))
+        self.assertEqual(content['success'], True)
+        # self.assertGreater(len(content['data']), 0)
+        self.assertEqual(
+            [i['indexI7Id'] for i in content['data']],
+            ['A05', 'A19', 'RP1'],
+        )
+
+    def test_one_sample(self):
+        self.client.login(email='foo@bar.io', password='foo-foo')
+        response = self.client.post(reverse('generate_indices'), {
+            'samples': '[%s]' % self.sample1.pk,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, 'utf-8'), {
+            'success': False,
+            'error': 'Select at least two samples.',
+            'data': [],
+        })
+
+    def test_missing_index_types(self):
+        self.client.login(email='foo@bar.io', password='foo-foo')
+        samples = ','.join(
+            [str(self.sample4.pk), str(self.sample5.pk)],
+        )
+        response = self.client.post(reverse('generate_indices'), {
+            'samples': '[%s]' % samples,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, 'utf-8'), {
+            'success': False,
+            'error': 'Index Type must be set for all libraries and samples.',
+            'data': [],
+        })
+
+    def test_missing_samples(self):
+        self.client.login(email='foo@bar.io', password='foo-foo')
+        response = self.client.post(reverse('generate_indices'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, 'utf-8'), {
+            'success': False,
+            'error': 'No samples.',
+            'data': [],
+        })
