1ecf847d58c4a1fb96e494198621a56fcd434476
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-05-11 21:41:28 +0200

    [WIP]: Password reset

--- common/urls.py
+++ common/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^get_navigation_tree/$', views.get_navigation_tree, name='get_navigation_tree'),
+
+    url(r'^accounts/password/reset/$', auth_views.password_reset, {'post_reset_redirect' : '/accounts/password/reset/done/'}),
+    url(r'^accounts/password/reset/done/$', auth_views.password_reset_done),
+    url(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'post_reset_redirect' : '/accounts/password/done/'}),
+    url(r'^accounts/password/done/$', auth_views.password_reset_complete),
 ]
