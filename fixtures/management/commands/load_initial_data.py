309030ed0170ac00b617402dac63913b972a0ff2
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-11-17 18:15:29 +0100

    [Index Generator] squash migrations

--- fixtures/management/commands/load_initial_data.py
+++ fixtures/management/commands/load_initial_data.py
@@ -3,30 +3,18 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Installs the fixtue(s) in the database.'
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--app',
-            action='store',
-            dest='app_label',
-            default=None,
-            help='Only look for fixtures in the specified app.',
-        )
+    help = 'Installs the fixture(s) in the database.'
 
     def handle(self, *args, **options):
-        app_label = options['app_label']
-
-        if app_label:
-            if app_label == 'flowcell':
-                self.load_flowcell_fixtures()
-
         # Load initial data for all apps
-        else:
-            self.load_flowcell_fixtures()
+        self.load_index_generator_fixtures()
+        self.load_flowcell_fixtures()
 
         self.stdout.write(self.style.SUCCESS(
             'Successfully loaded initial data.'))
+
+    def load_index_generator_fixtures(self):
+        call_command('loaddata', 'pool_sizes', app_label='index_generator')
 
     def load_flowcell_fixtures(self):
         call_command('loaddata', 'sequencers', app_label='flowcell')
