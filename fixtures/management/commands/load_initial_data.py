67b6a350163cf8957685e6ca5529d96c86d1fbfc
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-11-17 18:19:27 +0100

    [Sample] squash migrations

--- fixtures/management/commands/load_initial_data.py
+++ fixtures/management/commands/load_initial_data.py
@@ -7,11 +7,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Load initial data for all apps
+        self.load_sample_fixtures()
         self.load_index_generator_fixtures()
         self.load_flowcell_fixtures()
 
         self.stdout.write(self.style.SUCCESS(
             'Successfully loaded initial data.'))
+
+    def load_sample_fixtures(self):
+        call_command('loaddata', 'nucleic_acid_types', app_label='sample')
 
     def load_index_generator_fixtures(self):
         call_command('loaddata', 'pool_sizes', app_label='index_generator')
