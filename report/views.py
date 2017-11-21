9498a6c5e250f784d8a5aa6cff321df879af9d6b
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-11-21 12:18:54 +0100

    split main report function into smaller ones

--- report/views.py
+++ report/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
+from common.utils import timeit
 from common.models import Organization, PrincipalInvestigator
 from library_sample_shared.models import LibraryProtocol
 from library.models import Library
@@ -35,7 +36,38 @@ def report(request):
     data['start_date'] = start_date.strftime('%d.%m.%y')
 
     # Total Sample Count
-    data['total_counts'] = [
+    data['total_counts'] = get_total_counts(libraries, samples)
+
+    # Count by Organization
+    data['organization_counts'] = get_organization_counts(request_ids)
+
+    # Count by Library Protocol
+    data['protocol_counts'] = get_library_protocol_counts(request_ids)
+
+    principal_investigators = PrincipalInvestigator.objects.order_by(
+        'organization__name', 'name'
+    )
+
+    # Count by Principal Investigator
+    data['pi_counts'] = get_pi_counts(request_ids, principal_investigators)
+
+    sequencers = Sequencer.objects.all().order_by('name')
+
+    # Count by Sequencer
+    data['sequencer_counts'] = get_sequencer_counts(sequencers)
+
+    # Count by PI and Sequencer
+    data['sequencers_list'] = sequencers.values_list('name', flat=True)
+    data['libraries_on_sequencers_count'] = get_pi_sequencer_counts(
+        principal_investigators, sequencers,
+    )
+
+    return render(request, 'report.html', data)
+
+
+@timeit
+def get_total_counts(libraries, samples):
+    return [
         {
             'type': 'Samples',
             'count': samples.count(),
@@ -46,8 +78,10 @@ def report(request):
         }
     ]
 
-    # Count by Organization
-    rows = [
+
+@timeit
+def get_organization_counts(request_ids):
+    return [
         {
             'name': organization.name,
             'samples_count': Sample.objects.filter(
@@ -61,10 +95,11 @@ def report(request):
         }
         for organization in Organization.objects.all().order_by('name')
     ]
-    data['organization_counts'] = rows
 
-    # Count by Library Protocol
-    rows = [
+
+@timeit
+def get_library_protocol_counts(request_ids):
+    return [
         {
             'name': protocol.name,
             'samples_count': Sample.objects.filter(
@@ -78,13 +113,11 @@ def report(request):
         }
         for protocol in LibraryProtocol.objects.all().order_by('name')
     ]
-    data['protocol_counts'] = rows
 
-    # Count by Principal Investigator
-    principal_investigators = PrincipalInvestigator.objects.order_by(
-        'organization__name', 'name'
-    )
-    rows = [
+
+@timeit
+def get_pi_counts(request_ids, principal_investigators):
+    return [
         {
             'name': pi.name,
             'samples_count': Sample.objects.filter(
@@ -98,11 +131,11 @@ def report(request):
         }
         for pi in principal_investigators
     ]
-    data['pi_counts'] = rows
 
-    # Count by Sequencer
+
+@timeit
+def get_sequencer_counts(sequencers):
     rows = []
-    sequencers = Sequencer.objects.all().order_by('name')
     for sequencer in sequencers:
         samples_count = 0
         libraries_count = 0
@@ -119,12 +152,13 @@ def report(request):
             'libraries_count': samples_count + libraries_count,
             'runs_count': flowcells.count(),
         })
-    data['sequencer_counts'] = rows
+    return rows
 
-    # Count by PI and Sequencer
+
+@timeit
+def get_pi_sequencer_counts(principal_investigators, sequencers):
     # TODO: Highly nonoptimal and slow
     rows = []
-    data['sequencers_list'] = sequencers.values_list('name', flat=True)
     for pi in principal_investigators:
         row = OrderedDict({'pi': pi.name})
         for sequencer in sequencers:
@@ -144,6 +178,4 @@ def report(request):
                 ).count()
             row[sequencer.name] = samples_count + libraries_count
         rows.append(row)
-    data['libraries_on_sequencers_count'] = rows
-
-    return render(request, 'report.html', data)
+    return rows
