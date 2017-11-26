e38d10b8bcb7845f3f0bc3fb83816d11e3c69159
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-11-26 19:34:06 +0100

    fix a bug when a wrong id was returned

--- static/main-hub/app/store/flowcell/Flowcells.js
+++ static/main-hub/app/store/flowcell/Flowcells.js
@@ -1,51 +1,56 @@
 Ext.define('MainHub.store.flowcell.Flowcells', {
-    extend: 'Ext.data.Store',
-    storeId: 'flowcellsStore',
-    requires: ['MainHub.model.flowcell.Flowcell'],
-    model: 'MainHub.model.flowcell.Flowcell',
+  extend: 'Ext.data.Store',
+  storeId: 'flowcellsStore',
 
-    groupField: 'flowcell',
-    groupDir: 'DESC',
+  requires: [
+    'MainHub.model.flowcell.Flowcell'
+  ],
 
-    proxy: {
-        type: 'ajax',
-        // timeout: 1000000,
-        pageParam: false,   //to remove param "page"
-        startParam: false,  //to remove param "start"
-        limitParam: false,  //to remove param "limit"
-        noCache: false,     //to remove param "_dc",
-        api: {
-            read: 'api/flowcells/',
-            update: 'api/flowcells/edit/'
-        },
-        reader: {
-            type: 'json',
-            rootProperty: 'data',
-            successProperty: 'success',
-            messageProperty: 'message'
-        },
-        writer: {
-            type: 'json',
-            rootProperty: 'data',
-            transform: {
-                fn: function(data, request) {
-                    if (!(data instanceof Array)) {
-                        data = [data];
-                    }
+  model: 'MainHub.model.flowcell.Flowcell',
 
-                    var store = Ext.getStore('flowcellsStore');
-                    var newData = _.map(data, function(item) {
-                        var record = store.findRecord('id', item.id);
-                        if (record) {
-                            return Ext.Object.merge({
-                                pk: record.get('pk')
-                            }, record.getChanges());
-                        }
-                    });
-                    return newData;
-                },
-                scope: this
+  groupField: 'flowcell',
+  groupDir: 'DESC',
+
+  proxy: {
+    type: 'ajax',
+    timeout: 100000,
+    pageParam: false,   // to remove param "page"
+    startParam: false,  // to remove param "start"
+    limitParam: false,  // to remove param "limit"
+    noCache: false,     // to remove param "_dc",
+    api: {
+      read: 'api/flowcells/',
+      update: 'api/flowcells/edit/'
+    },
+    reader: {
+      type: 'json',
+      rootProperty: 'data',
+      successProperty: 'success',
+      messageProperty: 'message'
+    },
+    writer: {
+      type: 'json',
+      rootProperty: 'data',
+      transform: {
+        fn: function (data, request) {
+          if (!(data instanceof Array)) {
+            data = [data];
+          }
+
+          var store = Ext.getStore('flowcellsStore');
+          var newData = _.map(data, function (item) {
+            var record = store.findRecord('id', item.id, 0, false, true, true);
+            if (record) {
+              return Ext.Object.merge({
+                pk: record.get('pk')
+              }, record.getChanges());
             }
-        }
+          });
+
+          return newData;
+        },
+        scope: this
+      }
     }
+  }
 });
