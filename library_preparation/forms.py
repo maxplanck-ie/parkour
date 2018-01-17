ecf5a623a4eefb320ea08b371cf620f74b0c4a88
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2018-01-17 17:32:54 +0100

    delete obsolete forms

--- library_preparation/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import LibraryPreparation
-
-
-class LibraryPreparationForm(forms.ModelForm):
-    class Meta:
-        model = LibraryPreparation
-        exclude = ['sample']
--- pooling/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Pooling
-
-
-class PoolingForm(forms.ModelForm):
-    class Meta:
-        model = Pooling
-        fields = ('concentration_c1',)
