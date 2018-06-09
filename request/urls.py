51b632e98de73d682ddfdb13eca5d0c52c4c326d
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2018-06-09 20:54:37 +0200

    delete empty urls.py files

--- request/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from request import views
-
-
-urlpatterns = [
-    url(r'^get_files/$', views.get_files, name='get_files'),
-    url(r'^upload_files/$', views.upload_files, name='upload_files'),
-]
--- wui/urls.py
+++ wui/urls.py
@@ -29,15 +29,6 @@ urlpatterns = [
     url(r'^api/usage/', include('usage.urls')),
 
     url(r'', include('common.urls')),
-    url(r'', include('library_sample_shared.urls')),
-    url(r'library/', include('library.urls')),
-    url(r'sample/', include('sample.urls')),
-    url(r'request/', include('request.urls')),
-    url(r'library_preparation/', include('library_preparation.urls')),
-    url(r'pooling/', include('pooling.urls')),
-    url(r'flowcell/', include('flowcell.urls')),
-    url(r'invoicing/', include('invoicing.urls')),
-
     url(r'', include('report.urls')),
 ]
 
