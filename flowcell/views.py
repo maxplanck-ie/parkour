d587c7d6af90e8aeb04fc17c7dca62dc9e49cf8c
Thomas Manke <manke@mac1104.ie-freiburg.mpg.de>
2021-11-24 11:21:07 +0100

    flowcell/views.py: export sequencing_depth

--- flowcell/views.py
+++ flowcell/views.py
@@ -391,6 +391,7 @@ class FlowcellAnalysisViewSet(viewsets.ViewSet):
                     item.library_protocol.name,
                     item.organism.name,
                     item.index_type.name,
+                    item.sequencing_depth,
                 ]
 
         return Response(requests)
